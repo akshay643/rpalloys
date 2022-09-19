1d6d18c284f4ceb885c00e235852d5772a00caae
akshay643 <73953645+akshay643@users.noreply.github.com>
2022-09-19 11:36:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+  var nav = document.getElementById('nav')
+  var toggle = document.getElementById('toggle')
+  nav.classList.toggle('active')
+  toggle.classList.toggle('active')
+  console.log('clicked')
+}
